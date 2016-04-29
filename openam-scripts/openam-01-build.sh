c3f2db5012ef64c0f5baf01e4a44e1a086748804
Martin Gollogly <martin.gollogly@scisys.co.uk>
2016-04-29 10:53:24 +0100

    Update openam-01-build.sh

--- openam-scripts/openam-01-build.sh
+++ openam-scripts/openam-01-build.sh
@@ -3,16 +3,16 @@
 # Compilation of OpenAM
 # Author : Guy Godfroy, Linagora, 2015
 
-MVN=/opt/maven/current/bin/mvn
+#MVN=/opt/maven/current/bin/mvn
 
 source openam-config
 
 #### Patch maven configuration ####
 
-cp openam-server-prepare-war.xml $HOME/$OPENAM_FOLDER/openam-server-only/
+#cp openam-server-prepare-war.xml $HOME/$OPENAM_FOLDER/openam-server-only/
 
 #### Compilation ####
 cd $HOME/$OPENAM_FOLDER
-$MVN -DskipTests=true clean install
+mvn -DskipTests=true clean install
 cd openam-distribution/openam-distribution-ssoadmintools
-$MVN clean install
+mvn clean install
