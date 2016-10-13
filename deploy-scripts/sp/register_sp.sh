9e644b5d325d70c6ce8403f05a8cdc19a08e0851
yves.goupil <yves.goupil@meteo.fr>
2016-10-13 16:11:44 +0200

    register_sp.sh minor refactoring

--- deploy-scripts/sp/register_sp.sh
+++ deploy-scripts/sp/register_sp.sh
@@ -4,15 +4,15 @@ source ../deploy_config
   echo -e "\nretrieve identity card for user portal... "
   wget -O portal-meta.xml.user-portal "$USER_PORTAL_URL/saml2/jsp/exportmetadata.jsp"
   cp portal-meta.xml.user portal-meta.xml
-  echo "done"
-  echo -e "\nregister user portal service provider..."
+   echo -e "\nregister user portal service provider..."
   ./service-prov.sh
+  echo "done"
 
   echo -e "\nretrieve identity card for admin portal... "
   wget -O portal-meta.xml.admin-portal "$ADMIN_PORTAL_URL/saml2/jsp/exportmetadata.jsp"
   cp portal-meta.xml.admin portal-meta.xml
-  echo "done"
   echo -e "\nregister admin portal service provider..."
   ./service-prov.sh
+  echo "done"
 
 echo -e "\n*** Service providers registered ***"
